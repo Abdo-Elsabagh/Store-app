7258419dd9ca6aea6c81ebd77f17f5ffe5aab231
Abdo-Elsabagh <aelsabaggha2000@gmail.com>
2025-09-22 03:28:52 +0300

    `Added token support and PUT request functionality to Api class`

--- lib/helper/api.dart
+++ lib/helper/api.dart
@@ -4,7 +4,14 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
 class Api {
-  Future<dynamic> get({required String url}) async {
+  Future<dynamic> get({
+    required String url,
+    @required String? token,
+  }) async {
+    Map<String, String> headers = {};
+    if (token != null) {
+      headers.addAll({'Authorization': 'Bearer $token'});
+    }
     http.Response response = await http.get(Uri.parse(url));
     if (response.statusCode == 200) {
       return jsonDecode(response.body);
@@ -27,6 +34,29 @@ class Api {
     http.Response response =
         await http.post(Uri.parse(url), body: body, headers: headers);
     if (response.statusCode == 200 || response.statusCode == 201) {
+      Map<String, dynamic> data = jsonDecode(response.body);
+
+      return data;
+    } else {
+      throw Exception(
+          'there is a problem with status code ${response.statusCode} with body ${jsonDecode(response.body)}');
+    }
+  }
+
+  Future<dynamic> put({
+    required String url,
+    @required dynamic body,
+    @required String? token,
+  }) async {
+    Map<String, String> headers = {};
+    headers.addAll({'Content-Type': 'application/x-www-form-urlencoded'});
+    if (token != null) {
+      headers.addAll({'Authorization': 'Bearer $token'});
+    }
+
+    http.Response response =
+        await http.post(Uri.parse(url), body: body, headers: headers);
+    if (response.statusCode == 200) {
       Map<String, dynamic> data = jsonDecode(response.body);
 
       return data;
